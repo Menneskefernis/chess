1a657f3c51ce691afcd10b7f4f645dfc45e5a19e
Henrik Nørgaard <menneskefernis@gmail.com>
2020-01-11 17:47:47 +0100

    added methods for setting up pieces on the board

--- board.rb
+++ board.rb
@@ -1,5 +1,20 @@
 require_relative 'square'
 
+KING_WHITE = "\u{2654} "
+QUEEN_WHITE = "\u{2655} "
+ROOK_WHITE = "\u{2656} "
+BISHOP_WHITE = "\u{2657} "
+KNIGHT_WHITE = "\u{2658} "
+PAWN_WHITE = "\u{2659} "
+KING_BLACK = "\u{265A} "
+QUEEN_BLACK = "\u{265B} "
+ROOK_BLACK = "\u{265C} "
+BISHOP_BLACK = "\u{265D} "
+KNIGHT_BLACK = "\u{265E} "
+PAWN_BLACK = "\u{265F} "
+
+#"\u{2657} "
+
 class Board
   attr_accessor :state
   
@@ -26,17 +41,38 @@ class Board
   end
 
   def draw
+    puts ""
     (state[0].size - 1).downto(0) do |i|
       state.each do |row|
-        #row[i].piece = "\u{2657} " if row[i].x == 1 && row[i].y == 0
         print row[i]
       end
       puts ""
+    end
+    puts ""
+  end
+
+  def set_up_board
+    pieces_white = [ROOK_WHITE, KNIGHT_WHITE, BISHOP_WHITE, KING_WHITE, QUEEN_WHITE, BISHOP_WHITE, KNIGHT_WHITE, ROOK_WHITE]
+    pieces_black = [ROOK_BLACK, KNIGHT_BLACK, BISHOP_BLACK, QUEEN_BLACK, KING_BLACK, BISHOP_BLACK, KNIGHT_BLACK, ROOK_BLACK]
+    
+    add_pieces(1, PAWN_WHITE)
+    add_pieces(6, PAWN_BLACK)
+    add_pieces(0, pieces_white)
+    add_pieces(7, pieces_black)
+  end
+
+  def add_pieces(row, content)
+    8.times do |i|
+      if content.is_a? Array
+        state[i][row].piece = content[i]
+      else
+        state[i][row].piece = content
+      end
     end
   end
 end
 
 board = Board.new
+board.set_up_board
 board.draw
 
-#"\u{2657} "
