8a703bff4c0f2da30612ccd57492ce626ec4e51f
Henrik Nørgaard <menneskefernis@gmail.com>
2020-01-11 21:42:26 +0100

    create method for validating user input

--- game.rb
+++ game.rb
@@ -8,6 +8,7 @@ class Game
 
   def intro
     puts "This is Chess! A game for smart people... why don't you just stop here?"
+    puts
   end
 
   def start
@@ -18,13 +19,24 @@ class Game
   def get_input
     input = ""
     loop do
+      puts "Please provide a row and a column you would like to select, ex. 'B3'"
       input = gets.chomp
-      break if input_valid?
+      break if input_valid? input
     end
     input
   end
-  
+
+  def input_valid?(input)
+    input.downcase!
+    return false unless input.size == 2
+    
+    column, row = input.split('')
+    return false unless ("a".."h").include?(column)
+    return false unless (1..7).include?(row.to_i)
+    true
+  end
 end
 
 chess = Game.new
-chess.board.draw
+chess.start
+#chess.board.draw
