4755a0d2837b0beaabc0a9ee8e7c063006ef8c81
Henrik Nørgaard <menneskefernis@gmail.com>
2020-01-12 17:19:59 +0100

    fix bug where pawn could take opponent pawn in same column + nil bug for pawns in outer lanes checking for cross opponent

--- pieces/pawn.rb
+++ pieces/pawn.rb
@@ -16,7 +16,7 @@ class Pawn < Piece
     
     moves += opponent_squares(board, player)
 
-    moves << [x, y.send(operator, 1)]
+    moves << [x, y.send(operator, 1)] unless board.state[x][y.send(operator, 1)].piece
     moves << [x, y.send(operator, 2)] if first_move
     moves
   end
@@ -26,6 +26,7 @@ class Pawn < Piece
     cross_squares = find_cross_squares(board, player)
     
     cross_squares.each do |square|
+      next if square.nil?
       if square.piece.is_a? Piece
         moves << [square.x, square.y] unless board.state[square.x][square.y].piece.color == player.color
       end
@@ -40,15 +41,13 @@ class Pawn < Piece
 
     if player.color == "white"
       opponent_left = board.state[x - 1][y + 1]
-      opponent_right = board.state[x + 1][y + 1]
-    end
-    if player.color == "black"
-      opponent_left = board.state[x - 1][y - 1]
+      opponent_right = board.state[x + 1][y + 1] if x < 7
+    else
+      opponent_left = board.state[x - 1][y - 1] if x > 0
       opponent_right = board.state[x + 1][y - 1] 
     end
     
-    cross_squares << opponent_left
-    cross_squares << opponent_right
+    cross_squares.push(opponent_left, opponent_right)
   end
 
   def to_s
